9cf5393ab86925ee8609ab83195218f25b8bb049
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-10-22 14:10:56 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+      <v-app>
+        <v-content>
+          <v-toolbar>
+            <v-toolbar-title>Weather App</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-toolbar-items>
+              <v-btn text> <nuxt-link to="/" class="navbar-link">Home</nuxt-link> </v-btn>
+              <v-btn text>Link 2</v-btn>
+              <v-btn text>Link 3</v-btn>
+            </v-toolbar-items>
+          </v-toolbar>
+
+          <!-- content -->
+          <nuxt />
+        </v-content>
+      </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .navbar-link {
+    text-decoration: none;
+    color: white;
+  }
+</style>
